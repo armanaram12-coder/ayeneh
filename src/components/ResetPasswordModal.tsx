1dc709314cdf1232963eb40cc6a4f5a1d62ef432
arman-soravin <arman.aram12@gmail.com>
2026-08-23 16:15:09 +0330

    Update ResetPasswordModal.tsx

--- src/components/ResetPasswordModal.tsx
+++ src/components/ResetPasswordModal.tsx
@@ -24,7 +24,7 @@ export default function ResetPasswordModal({ isOpen, onClose }: ResetPasswordMod
 
     try {
       const { error } = await supabase.auth.resetPasswordForEmail(email, {
-        redirectTo: `${window.location.origin}/auth/reset-password`,
+        redirectTo: `https://ayeneh.vercel.app/auth/reset-password`,
       });
 
       if (error) {
